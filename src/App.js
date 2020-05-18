5ae640082ea863327c76a724bf1860b50d8b37fc
Dhanmoni Nath <dhanmoninath989@gmail.com>
2020-05-18 21:49:42 +0530

    local storage support added

--- src/App.js
+++ src/App.js
@@ -9,14 +9,27 @@ import NewPaletteForm from "./Components/NewPaletteForm";
 class App extends Component {
   constructor(props) {
     super(props);
+    const allPalettes = JSON.parse(window.localStorage.getItem("palettes"));
     this.state = {
-      palettes: seedColors,
+      palettes: allPalettes || seedColors,
     };
     this.savePalette = this.savePalette.bind(this);
   }
 
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState(
+      {
+        palettes: [...this.state.palettes, newPalette],
+      },
+      this.syncLocalStorage
+    );
+  }
+  //save data to local storage
+  syncLocalStorage() {
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(this.state.palettes)
+    );
   }
   findId(id) {
     return this.state.palettes.find((palette) => {
